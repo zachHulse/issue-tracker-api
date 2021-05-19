2a7b2885ca0dfd3cfed818aea5a62e88c62bfbeb
zachHulse <zhulse@accendero.com>
2021-05-19 13:11:17 -0600

    add email validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true,
+            format: { with: URI::MailTo::EMAIL_REGEXP }
   has_secure_password
 
   has_many :assignments
