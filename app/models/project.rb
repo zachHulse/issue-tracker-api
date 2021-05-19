c0f66e8de21e162adbf3af9d9da3e68060a5bc77
zachHulse <zhulse@accendero.com>
2021-05-19 15:29:11 -0600

    add abbreviation field to project

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,4 @@
 class Project < ApplicationRecord
   validates :name, presence: true, uniqueness: true
+  validates :code, presence: true, uniqueness: true, length: {maximum: 3}
 end
--- db/migrate/20210519200825_create_projects.rb
+++ db/migrate/20210519200825_create_projects.rb
@@ -3,6 +3,7 @@ class CreateProjects < ActiveRecord::Migration[6.1]
     create_table :projects do |t|
       t.string :name
       t.string :description
+      t.string :abbreviation
 
       t.timestamps
     end
