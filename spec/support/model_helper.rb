c9da3084cf2f0cfe056cca67d766c6bebdb13138
zachHulse <zhulse@accendero.com>
2021-05-19 16:15:53 -0600

    clean up model_helper.rb

--- spec/support/model_helper.rb
+++ spec/support/model_helper.rb
@@ -1,15 +1,15 @@
 module ModelHelper
   def test_is_required(model, field)
     model_instance = create(model)
-    model_instance[field] = NIL
+    model_instance[field] = nil
     expect(model_instance).to_not be_valid
     expect(
       model_instance.errors.messages[field]
     ).to include("can't be blank")
   end
   def test_is_unique(model, field)
-    instance_a = create(model, field => 'a')
-    instance_b = create(model, field => 'b')
+    instance_a = create(model)
+    instance_b = create(model)
     instance_b[field] = instance_a[field]
     expect(instance_b).to_not be_valid
     expect(instance_b.errors.messages[:name]).to include('has already been taken')
