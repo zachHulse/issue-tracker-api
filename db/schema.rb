bcf3fcb5013bf6b4cd20fddaf1e0910c438ee83e
zachHulse <zhulse@accendero.com>
2021-05-19 13:00:05 -0600

    change user password field name

--- db/schema.rb
+++ db/schema.rb
@@ -29,7 +29,7 @@ ActiveRecord::Schema.define(version: 2021_05_19_022314) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
