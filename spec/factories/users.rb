7fbb27f7e19e68d1c4c71f8b95929a2f140379e3
zachHulse <zhulse@accendero.com>
2021-05-19 13:21:51 -0600

    user model testing

---
+++ spec/factories/users.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :user do
+    email { "admin@example.com" }
+    password { "1234qwer" }
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  context "email" do
+    it "is required" do
+      user = create(:user)
+      user.email = NIL
+      expect(user).to_not be_valid
+      expect(user.errors.messages[:email]).to include("can't be blank")
+    end
+    it "should be unique" do
+      john = create(:user, email: 'john@place.com')
+      jane = create(:user, email: 'jane@place.com')
+      john.email = jane.email
+      expect(john).to_not be_valid
+      expect(john.errors.messages[:email]).to include('has already been taken')
+    end
+    it "should require email formatting" do
+      bad_emails = %w[bad bademail@ bad.com]
+      bad_emails.each {
+        |x|  user = User.new(email: x, password: '1234qwer')
+        expect(user).to_not be_valid
+        expect(user.errors.messages[:email]).to include('is invalid')
+      }
+    end
+  end
+  context "password" do
+    it "should be hashed" do
+      user = User.new(email: 'person@place.com', password: '1234qwer')
+      expect(user.password_digest).to be_truthy
+      expect(user.password_digest).to_not eq(user.password)
+    end
+  end
+end
