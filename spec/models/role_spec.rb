239158ff44c044157e9b80a6f058d3f9effe40fc
zachHulse <zhulse@accendero.com>
2021-05-18 19:46:53 -0600

    add test coverage for the role model

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Role, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "name" do
+    it "is required" do
+      role = create(:role)
+      role.name = NIL
+      expect(role).to_not be_valid
+      expect(role.errors.messages[:name]).to include("can't be blank")
+    end
+    it "should be unique" do
+      role_a = create(:role, name: 'a')
+      role_b = create(:role, name: 'b')
+      role_b.name = role_a.name
+      expect(role_b).to_not be_valid
+      expect(role_b.errors.messages[:name]).to include('has already been taken')
+    end
+  end
 end
