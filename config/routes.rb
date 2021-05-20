e532cbfbbdd3f2afdef951e73f3cecc2c5434967
zachHulse <zhulse@accendero.com>
2021-05-20 06:50:07 -0600

    add auth routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
+    resource :users, only: [:create]
+    post "/login", to: "users#login"
+    get "/auto_login", to: "users#auto_login"
     namespace :v1 do
-      resources :users
       resources :sprints
       resources :projects do
         resources :issues
