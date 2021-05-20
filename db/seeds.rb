4ce17d16b5fe5586b6d56c5b21ba93b57b1093d0
zachHulse <zhulse@accendero.com>
2021-05-20 06:50:17 -0600

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+admin_role = Role.create(name: :admin)
+dev_role = Role.create(name: :dev)
+
+admin = User.create(email: 'admin@example.com', password: '1234qwer')
+dev = User.create(email: 'dev@example.com', password: '1234qwer')
+
+Association.create(role: admin_role, user: admin)
+Association.create(role: dev_role, user: dev)
