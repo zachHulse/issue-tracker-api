a4a114ba2295fddfdf94198d42af105b5c8684a3
zachHulse <zhulse@accendero.com>
2021-05-20 00:51:26 -0600

    add issues and sprints to project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,7 @@
 class Project < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :abbreviation, presence: true, uniqueness: true, length: {maximum: 3}
+
+  has_many :issues
+  has_many :sprints
 end
