ecab16f6938ee2536c758de7e84e33144125fa81
zachHulse <zhulse@accendero.com>
2021-05-20 00:36:14 -0600

    flesh out projects_controller.rb

--- app/controllers/api/v1/projects_controller.rb
+++ app/controllers/api/v1/projects_controller.rb
@@ -1,2 +1,51 @@
 class Api::V1::ProjectsController < ApplicationController
+  before_action :find_project, only: [:show, :update, :destroy]
+
+  def index
+    authorize Project
+    @projects = Project.all
+    render json: @projects
+  end
+
+  def show
+    render json: @project
+  end
+
+  def create
+    authorize Project
+    @project = Project.new(project_params)
+    if @project.save
+      render json: @project
+    else
+      render error: { error: 'Unable to create project.' }, status: 400
+    end
+  end
+
+  def update
+    if @project
+      @project.update(project_params)
+      render json: @project
+    else
+      render json: { error: 'Unable to update fact.' }, status: 400
+    end
+  end
+
+  def destroy
+    if @project
+      @project.destroy
+      render json: { message: 'Project successfully deleted.' }, status: 200
+    else
+      render json: { error: 'Unable to delete project.' }, status: 400
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:name, :abbreviation).permit(:name, :abbreviation, :description)
+  end
+
+  def find_project
+    @project = authorize Project.find(params[:id])
+  end
 end
