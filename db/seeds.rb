3f053a16d33f41e793c97f3a1df4e0d23ac8e26d
zachHulse <zhulse@accendero.com>
2021-05-22 11:22:24 -0600

    show validation errors when seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
-admin_role = Role.create(name: :admin)
-dev_role = Role.create(name: :dev)
+admin_role = Role.create!(name: :admin)
+dev_role = Role.create!(name: :dev)
 
-admin = User.create(email: 'admin@example.com', password: '1234qwer')
-dev = User.create(email: 'dev@example.com', password: '1234qwer')
+admin = User.create!(email: 'admin@example.com', password: '1234qwer')
+dev = User.create!(email: 'dev@example.com', password: '1234qwer')
 
-Assignment.create(role: admin_role, user: admin)
-Assignment.create(role: dev_role, user: dev)
+admin.assignments.create!(role: admin_role)
+dev.assignments.create!(role: dev_role)
