7bbd25adf20dfa2326af37688bd401e89b06dae4
zachHulse <zhulse@accendero.com>
2021-05-22 09:31:15 -0600

    fix seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,5 @@ dev_role = Role.create(name: :dev)
 admin = User.create(email: 'admin@example.com', password: '1234qwer')
 dev = User.create(email: 'dev@example.com', password: '1234qwer')
 
-Association.create(role: admin_role, user: admin)
-Association.create(role: dev_role, user: dev)
+Assignment.create(role: admin_role, user: admin)
+Assignment.create(role: dev_role, user: dev)
